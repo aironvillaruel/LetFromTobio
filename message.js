7a760b0ace1722c76b86d67e6ff3285e7c15e866
Tobio <92922979+aironvillaruel@users.noreply.github.com>
2024-12-24 14:59:24 +0800

    Add files via upload

---
+++ message.js
@@ -0,0 +1,95 @@
+// window.editActivity = editActivity;
+function toAmaris() {
+    var bgAudio = document.getElementById("bgAudio");
+    document.getElementById("letterModal").style.display = "block";
+    document.getElementById("myModal").style.display = "none";
+    var letterContent = document.getElementById("letter-content");  // Get the <p> element
+    bgAudio.pause();
+    // Assign a message to the <p> element's textContent
+    letterContent.textContent = 'Hello ate Amaris, alam kong wala kayong ganitong event sa inyo but it supposed to be a message in the of the year so ayunn. Thank you dahil nagpapakupal ka sa akin every time HAHAHAHAH lakas ko mang gagi e no? I hope you are good and doing well sa mga pinaggagagawa mo sa buhay. You know na minsan lang ako maging serious so ayunnn padayon Teacher!! Domo Arigato.';
+
+    var audio = document.getElementById("myAudio");  // Replace "myAudio" with the ID of your <audio> element
+    audio.play().catch(error => {
+        console.log('Error playing audio:', error);
+    });
+}
+
+function toAja() {
+    console.log('Hello Aja hehehe bangis');
+    var bgAudio = document.getElementById("bgAudio");
+    document.getElementById("letterModal").style.display = "block";
+    document.getElementById("myModal").style.display = "none";
+    var letterContent = document.getElementById("letter-content");  // Get the <p> element
+    bgAudio.pause();
+    // Assign a message to the <p> element's textContent
+    letterContent.textContent = '"Alam mo joy, miss na miss na kita~~" HAHAHAHAHAHA jk so ayunn dito na ako gumawa ng message para sa mga admin ko. Fortunately may time pa gumawa ng website na ito HAHAHAHAHA I just wanna say thank you sa pagreceive ng aking pangungupal lakas manggagi eh no?. I am grateful that I met you this year, boss. Indeed, I am not really into words, but when I have a serious conversation, it is genuine. HAHAHAHAA so ayun stay healthy and huwag magpapastress masyado sa academics I will pray for your success boss. Dōmo arigatō ';
+
+    var audio = document.getElementById("marilag");  // Replace "myAudio" with the ID of your <audio> element
+    audio.play().catch(error => {
+        console.log('Error playing audio:', error);
+    });
+}
+
+function toCyRaH() {
+    console.log('Hello CyRaH hehehe lupitt');
+    var bgAudio = document.getElementById("bgAudio");
+    document.getElementById("letterModal").style.display = "block";
+    document.getElementById("myModal").style.display = "none";
+    var letterContent = document.getElementById("letter-content");  // Get the <p> element
+    bgAudio.pause();
+    // Assign a message to the <p> element's textContent
+    letterContent.textContent = 'Hello minmin, dito na ako sa LFT ko na ginawa babati HAHAHAHAH. Salamat sa pagtanggap ng aking pangungupal ngayong taon, I just wanna say na grateful ako kasi na meet kita though virtually lang but it is good at least na meet mo ang isang Tobio HEHEHEHE, and yes this letter and this website is exclusive lang para sa inyo. I hope you will appreciate this even though napakaliit na bagay, but the intention on this is so pure and genuine ayunn lang. Happy holiday Dōmo arigatō ';
+
+    var audio = document.getElementById("marilag");  // Replace "myAudio" with the ID of your <audio> element
+    audio.play().catch(error => {
+        console.log('Error playing audio:', error);
+    });
+}
+
+function toLuna() {
+    console.log('Hello Luna hehehe angass');
+    var bgAudio = document.getElementById("bgAudio");
+    document.getElementById("letterModal").style.display = "block";
+    document.getElementById("myModal").style.display = "none";
+    var letterContent = document.getElementById("letter-content");  // Get the <p> element
+    bgAudio.pause();
+    // Assign a message to the <p> element's textContent
+    letterContent.textContent = 'Wazzupp luna/sky high pegion fly HSHAHSHAHSA jk, so ayun dito na ako sa LFT babati I made this website para sa inyong mga virtual companion ko. I just wanna say thank youu sa pagtanggap ng aking pang gagagi, I am not magaling sa words but when I got serious you know it is genuine. I hope you are doing well palagi, if u got a problem and I am reachable do not hesitate na tawagin ako Tobio to e. So ayunn lang you are always appreciated, happy holiday at Dōmo arigatō';
+
+    var audio = document.getElementById("marilag");  // Replace "myAudio" with the ID of your <audio> element
+    audio.play().catch(error => {
+        console.log('Error playing audio:', error);
+    });
+}
+
+function toSiri() {
+    console.log('Hello Siri hehehe cool');
+    var bgAudio = document.getElementById("bgAudio");
+    document.getElementById("letterModal").style.display = "block";
+    document.getElementById("myModal").style.display = "none";
+    var letterContent = document.getElementById("letter-content");  // Get the <p> element
+    bgAudio.pause();
+    // Assign a message to the <p> element's textContent
+    letterContent.textContent = 'Yooo Siri , Treng , Iris , Avi basta lahat na HAHAHAHAHA jk, as I have said kagabi dito ko kayo babatiin I just wanna say na you are always appreciated hindi ko man maipakita in action but the importance of something and specialty of it is the intention, so ayun maintain your traits napaka cool ng ganiyan. I will pray for your success, sorry hindi ako magaling sa pagmotivate AHHAHAHA but when I say something it means special and genuine. Happy holiday sa inyo ng family moo Dōmo arigatō';
+
+    var audio = document.getElementById("myAudio");  // Replace "myAudio" with the ID of your <audio> element
+    audio.play().catch(error => {
+        console.log('Error playing audio:', error);
+    });
+}
+
+function toCiao() {
+    console.log('Hello Ciao hehehe cute');
+    var bgAudio = document.getElementById("bgAudio");
+    document.getElementById("letterModal").style.display = "block";
+    document.getElementById("myModal").style.display = "none";
+    var letterContent = document.getElementById("letter-content");  // Get the <p> element
+    bgAudio.pause();
+    // Assign a message to the <p> element's textContent
+    letterContent.textContent = 'Halluuu ciaoo, doc, ciarra-san so ayun aware ka na dito ako babati sa LFT AHAHAHA this is my first time na gumawa ng website para lang bumati eyy maximizing my skills. I just wanna say thank you palagii and you are always appreciated lalo na sa akin and salamat sa pagtanggap na aking mga kakupalan. I hope na nasa maayos kang kalagayan every single day lalo na at malayo ka sa family mo kapag may pasok. I know na you are always doing your best sa lahat ng bagay, ipapanalangin kita ayun lang. Happy holiday sa inyo ng family moo Dōmo arigatō';
+
+    var audio = document.getElementById("myAudio");  // Replace "myAudio" with the ID of your <audio> element
+    audio.play().catch(error => {
+        console.log('Error playing audio:', error);
+    });
+}
